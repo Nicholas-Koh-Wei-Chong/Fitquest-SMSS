c6cbea9fe5908e55ca844d5dc9a8482e052ce44d
Nicholas-Koh-Wei-Chong <koh_wei-chong_nicholas@moe.edu.sg>
2026-06-30 12:02:04 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 console.log("FitQuest Loaded");
+document.getElementById("startBtn").addEventListener("click", function(){
+
+    const name = document.getElementById("name").value;
+
+    if(name===""){
+
+        alert("Please enter the student's name.");
+
+        return;
+
+    }
+
+    alert("Welcome to FitQuest!\n\nThe NAPFA Assessment module will be added in the next milestone.");
+
+});
